3e539f9a7b02334baa4b91d814ad49c174c627cf
hbbdqy <986449846@qq.com>
2019-03-05 23:17:37 +0800

    没电了，先这样

---
+++ Sample05/Sample05/Content.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sample05
+{
+    public class Content
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string content { get; set; }
+        public int status { get; set; }
+        public DateTime add_time { get; set; } = DateTime.Now;
+        public DateTime? modify_time { get; set; }
+    }
+
+    public class Comment
+    {
+        public int id { get; set; }
+        public int content_id { get; set; }
+        public string content { get; set; }
+        public DateTime add_time { get; set; } = DateTime.Now;
+    }
+}
---
+++ Sample05/Sample05/Program.cs
@@ -0,0 +1,61 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+namespace Sample05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            test_insert();
+            test_mult_insert();
+        }
+
+        /// <summary>
+        /// 插入一条数据
+        /// </summary>
+        static void test_insert()
+        {
+            var content = new Content
+            {
+                title = "标题1",
+                content = "这是内容",
+            };
+            using (var conn = new MySqlConnection("server=127.0.0.1;user=root;password=admin; database=coretest;"))
+            {
+                string sql_insert = @"insert into content(title,content,status,add_time,modify_time) values(@title,@content,@status,@add_time,@modify_time)";
+                var result = conn.Execute(sql_insert, content);
+                Console.WriteLine($"test_insert:插入了{result}条数据");
+            }
+        }
+
+        static void test_mult_insert()
+        {
+            List<Content> contents = new List<Content>() {
+                   new Content
+                {
+                    title = "批量插入标题1",
+                    content = "批量插入内容1",
+
+                },
+                   new Content
+                {
+                    title = "批量插入标题2",
+                    content = "批量插入内容2",
+
+                },
+            };
+
+            using (var conn = new MySqlConnection("server=127.0.0.1;user=root;password=admin; database=coretest;"))
+            {
+                string sql_insert = @"insert into content(title,content,status,add_time,modify_time) values(@title,@content,@status,@add_time,@modify_time)";
+                var result = conn.Execute(sql_insert, contents);
+                Console.WriteLine($"test_mult_insert：插入了{result}条数据！");
+            }
+        }
+    }
+}
