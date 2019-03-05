73455c039c7ed7d8b9fb44e8d254bb9ec80e6005
hbbdqy <986449846@qq.com>
2019-03-05 22:02:54 +0800

    .net core练习，首次提交

---
+++ Sample02/DotNetCoreCMS/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace DotNetCoreCMS
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostContext,config)=> {
+                    var env = hostContext.HostingEnvironment;
+                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                          .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                          .AddJsonFile("Content.json", optional:false,reloadOnChange: true)
+                          .AddEnvironmentVariables();
+                })
+                .UseStartup<Startup>(); //指定setup类
+    }
+}
